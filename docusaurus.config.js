6b45c960324d68299ceab7e94576670d56f434c5
clementreiffers <reiffersclement@gmail.com>
2023-07-18 17:55:05 +0200

    update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: "https://clementreiffers.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/octo-workers-docs/build/index.html/",
+  baseUrl: "/octo-workers-docs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
